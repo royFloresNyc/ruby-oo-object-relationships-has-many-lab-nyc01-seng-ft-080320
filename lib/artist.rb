f8c172a406eca450e7458a2c0b1faf5d5f408cf8
royFloresNyc <royfloresnyc@gmail.com>
2020-08-05 20:17:18 -0400

    Define Artist and Song class with relatinships

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+require_relative "../lib/song.rb"
+
+class Artist
+
+  attr_accessor :name   
+
+  def initialize(name)
+    @name = name 
+  end 
+
+  def songs
+    Song.all.filter do |songs|
+      songs.artist == self 
+    end 
+  end 
+
+  def add_song(song)
+    song.artist = self
+  end 
+
+  def add_song_by_name(song_name)
+     song = Song.new(song_name)
+     song.artist = self 
+  end 
+
+  def self.song_count
+    Song.all.count
+  end 
+
+end 
